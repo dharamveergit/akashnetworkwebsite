d60cd2b2c4e7436108fc848810851512dd1577c9
dharamveergit <dharamveersingh.em@gmail.com>
2025-12-10 00:11:15 +0530

    refactor: enhance font loading and CSS rendering performance
    - Implemented media="print" technique for font stylesheets to improve loading efficiency.
    - Added a fallback script to ensure fonts load correctly even if onload events fail.
    - Deferred non-critical CSS loading to enhance Largest Contentful Paint (LCP) and reduce render blocking.

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,13 +14,17 @@ import { redirects } from "./src/utils/redirects";
 export default defineConfig({
   redirects: redirects,
   markdown: {
-    remarkPlugins: [remarkMath, normalizeMath, remarkDirective, mermaid, customAsidePlugin],
+    remarkPlugins: [
+      remarkMath,
+      normalizeMath,
+      remarkDirective,
+      mermaid,
+      customAsidePlugin,
+    ],
   },
   integrations: [
     tailwind(),
-    sitemap({
-      lastmod: new Date("2024-06-27"),
-    }),
+    sitemap({ lastmod: new Date("2024-06-27") }),
     react(),
     astroExpressiveCode({
       themes: ["light-plus", "dark-plus"],
@@ -38,4 +42,8 @@ export default defineConfig({
     mdx(),
   ],
   site: "https://akash.network",
+  vite: {
+    build: { cssCodeSplit: true, cssMinify: "lightningcss" },
+    css: { devSourcemap: false },
+  },
 });
